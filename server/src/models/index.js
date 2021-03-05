3c6ddaa4d89fad3b00854f4cd988f816bfe00708
Rodrigo Nogueira <rdg6design@gmail.com>
2021-03-05 05:00:03 -0300

    Dica para mandar a imagem

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,3 +1,11 @@
 require('./config');
 
 const Schema = require('./schema')
+
+// O tipo Buffer é usado para salva arquivos e retorna-los daforma, de Buffer, que conhecemos no Node.js, porem o MOngodb converte para Binary
+
+// const imageSchema = new Schema({
+//     mime: String,
+//     bin: Buffer
+// })
+
