ecf07ab5d04a81070c9ed228e9262e71af45dde5
Rodrigo Nogueira <rdg6design@gmail.com>
2021-03-06 17:49:55 -0300

    findOne

---
+++ server/src/models/findOne.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const _schema = {
+    name: String
+}
+
+const customerSchema = new Schema(_schema);
+const CustomerModel = mongoose.model('Customer', customerSchema);
+
+const query = {};
+
+CustomerModel.findOne(query, function (err, data) {
+    if(err) return console.log('ERRO: ', err);
+    return console.log('Buscou: ', data);
+})
+
+module.exports = CustomerModel;
