6053070840b8657bba5d964069969f7390a95297
Rodrigo Nogueira <rdg6design@gmail.com>
2021-03-06 22:03:32 -0300

    server

--- server/src/routes.js
+++ server/src/routes.js
@@ -25,41 +25,12 @@ router.post('/authenticate', (req, res) => {
 router.use(authMiddleware);
 
 router.get('/users', async (req, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: 'Rodrigo Nogueira',
-      website: 'https://homeacademy.com.br',
-    },
-    {
-      id: 2,
-      name: 'Mark Zuckerberg',
-      website: 'https://facebook.com',
-    },
-    {
-      id: 3,
-      name: 'Bill Gates',
-      website: 'https://www.microsoft.com',
-    },
-    {
-    id: 4,
-    address: {
-      country: 'USA',
-      state: 'Texas',
-      city: 'Dallas',
-      street: '75247'
-    },
-    avatarUrl: '/static/images/avatars/avatar_6.png',
-    createdAt: 1554757200000,
-    email: 'clarke.gillebert@devias.io',
-    name: 'Clarke Gillebert',
-    phone: '972-333-4106'
-    },
-  ]);
-});
-
-router.post('/users', async (req, res) => {
-  console.log('Hi')
+    const db = require('../db');
+    const Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+    Customer.find({}).lean().exec(function(e, docs){
+      res.json(docs);
+      res.end();
+    })
 });
 
 module.exports = router;
