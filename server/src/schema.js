ef685c023f5e69091acc5dba3fd5916c1b3ba7af
Rodrigo Nogueira <rdg6design@gmail.com>
2021-03-06 22:01:07 -0300

    schema

---
+++ server/src/schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const _schema ={
+    name: String,
+    email: String,
+    phone: Number,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    avatarUrl: String,
+    address: {
+        country: String,
+        state: String,
+        city: String,
+        street: String
+      }
+}
+
+module.exports = new Schema(_schema);
